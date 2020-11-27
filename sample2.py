e67c44877cae5498024211807ade1c87fc38cbf8
ms12334 <to.totoro@hotmail.com>
2020-11-26 20:57:21 -0500

    Update sample2.py

--- sample2.py
+++ sample2.py
@@ -3,6 +3,6 @@ pattern = re.compile(r'(?:<genre>)(?P<genre>.*?)(?:</genre>)',re.DOTALL)
 with open('books.xml','r') as fin:
     with open('result.txt','w') as fout:
         str = fin.read()
-        results = re.findall(pattern,str)
-        for result in results:
-            fout.write(result + '\n')
+        matches = re.findall(pattern,str)
+        for match in matches:
+            fout.write(match + '\n')
